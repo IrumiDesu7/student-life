802dc8c6e7f7f7bbadacf594894c4b2ea693d43e
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2022-07-26 20:25:52 +0800

    change port

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 5000
+const port = 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
