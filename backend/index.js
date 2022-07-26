6777806066636f44c3457f5155350e61fef9dc0e
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2022-07-26 20:34:18 +0800

    add cors to index

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 3000
+const cors = require('cors')
+const port = 4000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended : true
