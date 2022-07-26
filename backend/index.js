06976a728933a5fd8304a2b9d562cabc1290692d
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2022-07-26 20:17:00 +0800

    add login and signup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended : true
+}))
+
+app.get('/api/login', (req, res) => {
+  const data = {
+    username : req.body.username,
+    password : req.body.password
+  }
+  return res.send(data)
+})
+
+app.post('/api/signup', (req, res) => {
+  const data = {
+    email : req.body.email,
+    password : req.body.password,
+    password2 : req.body.password2,
+  }
+  return res.send(data)
 })
 
 app.listen(port, () => {
