07df360d5e2b8a724fec0f9780136f2a5742a201
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2022-10-22 15:12:50 +0800

    delete BE repo

--- backend/utils/db.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-const URI = process.env.MONGODB_URL
-const DB = 'student'
-
-mongoose.connect(`${URI}/${DB}`, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-})
