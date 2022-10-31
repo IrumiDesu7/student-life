2997796d51987227710db687735ca8fcc5c17f39
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2022-10-31 18:51:27 +0800

    add authentication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,16 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './login-signup.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+import "./login-signup.css";
 
 function Login() {
   const [loginData, setLoginData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
+  const navigate = useNavigate();
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -20,46 +24,52 @@ function Login() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(loginData);
+    axios
+      .post("https://student-life-be.vercel.app/api/login", loginData)
+      .then((res) => {
+        const token = res.data.data.jwt;
+        localStorage.setItem("token", token);
+        if (token) navigate("/");
+      });
   }
 
   return (
-    <div className='login-container'>
+    <div className="login-container">
       <img
-        className='login-image'
-        src='images/login-image.jpg'
-        alt='students'
+        className="login-image"
+        src="images/login-image.jpg"
+        alt="students"
       />
-      <div className='right-side'>
-        <h3 className='login-title'>Student Life</h3>
-        <p className='login-description'>
+      <div className="right-side">
+        <h3 className="login-title">Student Life</h3>
+        <p className="login-description">
           Welcome, students! Enter your credentials to access this website
         </p>
-        <form onSubmit={handleSubmit} className='form-container'>
-          <label htmlFor='email'>Email</label>
+        <form onSubmit={handleSubmit} className="form-container">
+          <label htmlFor="email">Email</label>
           <input
-            type='email'
-            name='email'
-            id='email'
-            placeholder='Enter your email'
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
             required={true}
             onChange={handleChange}
             value={loginData.email}
           />
-          <label htmlFor='password'>Password</label>
+          <label htmlFor="password">Password</label>
           <input
-            type='password'
-            name='password'
-            id='password'
+            type="password"
+            name="password"
+            id="password"
             required
-            placeholder='Enter your password'
+            placeholder="Enter your password"
             onChange={handleChange}
             value={loginData.password}
           />
-          <button className='login-btn'>Login</button>
+          <button className="login-btn">Login</button>
         </form>
         <p>
-          If you're new, create your account <Link to='/signup'>here</Link>!
+          If you're new, create your account <Link to="/signup">here</Link>!
         </p>
       </div>
     </div>
--- src/components/Login/Signup.js
+++ src/components/Login/Signup.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Signup() {
   const [signupData, setSignupData] = useState({
-    email: '',
-    password: '',
-    passwordConfirm: '',
+    email: "",
+    password: "",
+    password_confirm: "",
   });
 
   function handleChange(e) {
@@ -21,10 +21,10 @@ function Signup() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    const { password, passwordConfirm } = signupData;
-    if (password === passwordConfirm) {
+    const { password, password_confirm } = signupData;
+    if (password === password_confirm) {
       axios
-        .post('http://localhost:4000/api/signup', signupData)
+        .post("https://student-life-be.vercel.app/api/register", signupData)
         .then((res) => console.log(res));
     } else {
       console.log("password isn't the same");
@@ -32,52 +32,52 @@ function Signup() {
   }
 
   return (
-    <div className='login-container'>
+    <div className="login-container">
       <img
-        className='login-image'
-        src='images/login-image.jpg'
-        alt='students'
+        className="login-image"
+        src="images/login-image.jpg"
+        alt="students"
       />
-      <div className='right-side'>
-        <h3 className='login-title'>Student Life</h3>
-        <p className='login-description'>
+      <div className="right-side">
+        <h3 className="login-title">Student Life</h3>
+        <p className="login-description">
           Welcome, students! Create new account to access this website
         </p>
-        <form onSubmit={handleSubmit} className='form-container'>
-          <label htmlFor='email'>Email</label>
+        <form onSubmit={handleSubmit} className="form-container">
+          <label htmlFor="email">Email</label>
           <input
-            type='email'
-            name='email'
-            id='email'
-            placeholder='Enter your email'
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
             required={true}
             onChange={handleChange}
             value={signupData.email}
           />
-          <label htmlFor='password'>Password</label>
+          <label htmlFor="password">Password</label>
           <input
-            type='password'
-            name='password'
-            id='password'
+            type="password"
+            name="password"
+            id="password"
             required
-            placeholder='Enter your password'
+            placeholder="Enter your password"
             onChange={handleChange}
             value={signupData.password}
           />
-          <label htmlFor='passwordConfirm'>Confirm Password</label>
+          <label htmlFor="password_confirm">Confirm Password</label>
           <input
-            type='password'
-            name='passwordConfirm'
-            id='passwordConfirm'
+            type="password"
+            name="password_confirm"
+            id="password_confirm"
             required
-            placeholder='Confirm your password'
+            placeholder="Confirm your password"
             onChange={handleChange}
-            value={signupData.passwordConfirm}
+            value={signupData.password_confirm}
           />
-          <button className='login-btn'>Signup</button>
+          <button className="login-btn">Signup</button>
         </form>
         <p>
-          Already have an account? Click <Link to='/login'>here</Link>!
+          Already have an account? Click <Link to="/login">here</Link>!
         </p>
       </div>
     </div>
